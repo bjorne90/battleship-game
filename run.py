416ffe4071c0e1bd6728014b072094337441d3ab
bjorne90 <bjce90@gmail.com>
2023-04-06 14:09:08 +0000

    Trackback the code, bug when trying to fix Computers board

--- run.py
+++ run.py
@@ -135,13 +135,12 @@ def print_boards(player_board, computer_board, name):
     Print the player and computer boards side by side.
     """
     hidden_computer_board = [
-        ['0' if (cell == 'S' and (i, j) not in computer_board.hits)
-         else cell for j, cell in enumerate(row)]
-        for i, row in enumerate(computer_board.grid)]
+        ['0' if cell == 'S' else cell for cell in row]
+        for row in computer_board]
 
     print(f"{name}'s Board:")
     print("  " + " ".join(str(i) for i in range(len(player_board[0]))))
-    for i, row in enumerate(player_board.grid):
+    for i, row in enumerate(player_board):
         print(str(i) + " " + " ".join(
             cell if cell in ('0', 'H', 'M') else '0'
             for cell in row))
